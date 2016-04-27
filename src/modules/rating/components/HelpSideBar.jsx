0aec91876226261d6d283b595d64167f97b26805
Nikolay Kozhukharenko <nikolay.kozhuharenko@gmail.com>
2016-04-27 08:55:32 +0300

    move rating FAQ to sidebar

---
+++ src/modules/rating/components/HelpSideBar.jsx
@@ -0,0 +1,35 @@
+
+import React, {Component} from 'react';
+import {stack as Menu} from 'react-burger-menu';
+
+export default class HelpSideBar extends Component {
+  
+  static PropTypes = {
+    closeHelperSidebar: React.PropTypes.func.isRequired,
+    isOpen: React.PropTypes.bool.isRequired
+  };
+  
+  stateChanged = (state) => {
+    if (!state.isOpen) {
+      this.props.close()
+    }
+  };
+
+  render() {
+    console.log('HelpSideBar: ', this.props);
+    return (
+      <Menu right onStateChange={this.stateChanged}  isOpen={this.props.isOpen} width={300}>
+        <div>
+          1. Червона лінія відмежовує ті заявки з прохідним балом на
+          бюджетну форму навчання. Всі заявки які вище червоної межі
+          проходять, нижче - ні
+        </div>
+        <br/>
+        <div>
+          2. Синя лінія відмежовує заявки які можуть будти зараховані
+          на конктрактну форму навчання.
+        </div>
+      </Menu>
+    )
+  }
+}
--- src/modules/rating/components/Rating.jsx
+++ src/modules/rating/components/Rating.jsx
@@ -1,18 +1,29 @@
 'use strict';
 
 import React, {Component, PropTypes} from 'react';
-import Help from '../components/Help'
+import HelpSideBar from './HelpSideBar'
+import HelpBtn from './HelpBtn'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {toggleHelperSidebar, toggleGenderStatSidebar} from '../actions'
 
-export default class Rating extends React.Component {
-  render () {
+import '../styles/side-bar-right.styl'
+
+class Rating extends React.Component {
+  render() {
+    console.log('this.props', this.props);
     const { SpecofferChooser, RatingList, SearchEnrolment } = this.props;
+    const { openHelperSidebar, closeHelperSidebar, helpIsOpen } = this.props;
+    // const { openGenderStatSidebar, closeGenderStatSidebar, genderStatIsOpen } = this.props;
+
     return (
       <div className="rating">
+        <HelpSideBar isOpen={helpIsOpen} close={closeHelperSidebar} />
         <div className="rating__specoffer-chooser">
           {SpecofferChooser}
         </div>
         <div className="rating__rating-list">
-          <Help/>
+          <HelpBtn openHelperSidebar={openHelperSidebar}/>
           <div>{SearchEnrolment}</div>
           <div>{RatingList}</div>
         </div>
@@ -20,3 +31,17 @@ export default class Rating extends React.Component {
     )
   }
 }
+
+export default connect((state) => {
+  return {
+    helpIsOpen: state.rating.sideBar.helpIsOpen,
+    genderStatIsOpen: state.rating.sideBar.genderStatIsOpen,
+  }
+}, (dispath) => {
+  return bindActionCreators({
+    openHelperSidebar: toggleHelperSidebar(),
+    closeHelperSidebar: toggleHelperSidebar(false),
+    openGenderStatSidebar: toggleGenderStatSidebar,
+    closeGenderStatSidebar: toggleGenderStatSidebar(false),
+  }, dispath)
+})(Rating)
